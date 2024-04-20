017eb2cb056a2dae6e973d0630b5186b2467d45c
LucasNBS <lucasnunesbarretosampaio@gmail.com>
2024-04-20 09:23:05 -0300

    refactor: Fixing user manager

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from .managers import UserManager
 
 ROLES = (
   ("CEO", 'ceo'),
@@ -11,9 +12,11 @@ ROLES = (
 class User(AbstractUser):
   name = models.CharField(max_length=25)
   email = models.EmailField(unique=True)
-  password = models.CharField(max_length=20)
+  password = models.CharField(max_length=100)
   role = models.CharField(max_length=10, choices=ROLES, default="COMMON")
   username = None
+
+  objects = UserManager()
 
   USERNAME_FIELD = 'email'
   REQUIRED_FIELDS = ['name', 'password']
